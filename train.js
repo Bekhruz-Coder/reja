1ed7adbf6c59845b1e5eb15ec9ae5b3c4ad1d321
Bekhruz-Coder <bekhruzbek2022@gmail.com>
2025-09-25 14:50:59 +0900

    fix: refactoring

--- train.js
+++ train.js
@@ -1,34 +1,54 @@
 console.log("TRAIN AREA!");
-// A-Task
-console.log("A-Task");
+// // A-Task
+// console.log("A-Task");
 
-// Harf sifatida kiritilgan birinchi parametr, 
-// kiritilgan ikkinchi parametr tarkibida nechta ekanligini qaytaruvchi
-// Funktsiya tuzing
+// // Harf sifatida kiritilgan birinchi parametr, 
+// // kiritilgan ikkinchi parametr tarkibida nechta ekanligini qaytaruvchi
+// // Funktsiya tuzing
 
-// Masalan: countLetter("e", "engineer")
-// 'engineer' so'zi tarkibida 'e' harfi 3 marotaba takrorlanganligi uchun
-// 3 sonini qaytaradi
+// // Masalan: countLetter("e", "engineer")
+// // 'engineer' so'zi tarkibida 'e' harfi 3 marotaba takrorlanganligi uchun
+// // 3 sonini qaytaradi
 
-function countLetter(letter, text) {
-    let count = 0;
+// function countLetter(letter, text) {
+//     let count = 0;
 
-    if(typeof text !== 'string'){
-        console.log("Please enter String format text.");
-    } else {
-        let arr1 = [];
-        arr1 = text.split("");
-        const size = arr1.length;
-        for(let i = 0; i <= size; i++){
-            if(letter == arr1[i]) {
-                count++;
-            }
-        }
-        console.log(`"${text}" so'zida "${letter}" harifi ${count} marotaba takrorlandi.`);
-        console.log("Natija: ", count);
-    }
-}
-countLetter("e", "engineer");
+//     if(typeof text !== 'string'){
+//         console.log("Please enter String format text.");
+//     } else {
+//         let arr1 = [];
+//         arr1 = text.split("");
+//         const size = arr1.length;
+//         for(let i = 0; i <= size; i++){
+//             if(letter == arr1[i]) {
+//                 count++;
+//             }
+//         }
+//         console.log(`"${text}" so'zida "${letter}" harifi ${count} marotaba takrorlandi.`);
+//         console.log("Natija: ", count);
+//     }
+// }
+// countLetter("e", "engineer");
+
+// B-Task
+// function countDigits(text){
+//     let count = 0;
+//     if(typeof text !== "string"){
+//         console.log("Please enter the String");
+//         return;
+//     } else {
+//         let arr = [];
+//         arr = text.split('');
+//         for(let i = 0; i < arr.length; i++){
+//             if(arr[i] >= 0 && arr[i] <= 9){
+//                 count++;
+//             }
+//         }
+//     }
+//     console.log(`Yuqoridagi string tarkibida ${count} dona raqam qatnashganligi uchun, natija: ${count}`);
+// }
+
+// countDigits("ad2a54y79wet0sfgb9");
 
 
 
@@ -108,6 +128,56 @@ countLetter("e", "engineer");
 //     javob = await maslahatBering2(63);
 //     console.log(javob);
 //     javob = await maslahatBering2(42);
+//     console.log(javob);
+// }
+// run();
+
+console.log("Jack Ma maslahatlari!");
+const list = [
+    "yaxshi talaba bo'ling", // 0~20
+    "to'g'ri boshliq tanglang va ko'roq xato qiling", // 20~30
+    "o'zingizga ishlashingizni boshlang", // 30~40
+    "siz kuchli bo'lgan narsalarni qiling", // 40~50
+    "yoshlarga investitsiya qiling", // 50~60
+    "endi dam oling, foydasi yoq endi" // 60~
+];
+
+async function maslahatBering(a) {
+    if (typeof a !== "number") throw new Error("insert a number");
+    else if (a > 60) return list[0];
+    else if (a > 20 && a <= 30) return list[1];
+    else if (a > 30 && a <= 40) return list[2];
+    else if (a > 40 && a <= 50) return list[3];
+    else if (a > 50 && a <= 60) return list[4];
+    else {
+        return new Promise((resolve, reject) => {
+            setTimeout(() => {
+                resolve(list[5]);
+            }, 5000)
+        });
+    }
+}
+
+// then/catch
+console.log("passed here 0");
+maslahatBering(44)
+    .then((data) => {
+        console.log("javob:", data);
+    })
+    .catch((err) => {
+        console.log("ERROR:", err);
+    });
+
+console.log("passed here 1");
+
+
+// async/await
+// async function run() {
+//     let javob = await maslahatBering(20);
+//     console.log(javob);
+//     javob = await maslahatBering(63);
+//     console.log(javob);
+//     javob = await maslahatBering(42);
 //     console.log(javob);
 // }
 // run();
